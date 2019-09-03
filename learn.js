89952cc4b68b395e0655367e02a3374e409261e6
zhangjian <zhangjian@yuewen.com>
2019-09-03 23:51:04 +0800

    maopao

--- learn.js
+++ learn.js
@@ -139,6 +139,19 @@ function clone2(target, map = new WeakMap()) {
 	}
 }
 
+// 冒泡排序
+// 
+// 
+function popSort(arr) {
+	for (let i = 0; i < arr.length; i++ ){
+		for(let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+			}
+		}
+	}
+	return arr
+}
 
 
 
