bfca74e8b19084a64f6b34447c45c4bf6822b85c
zhangjian <zhangjian@yuewen.com>
2019-09-05 23:05:09 +0800

    template

--- learn.js
+++ learn.js
@@ -186,3 +186,20 @@ function throttle(fn, delay = 500) {
 	}
 }
 
+// 模版引擎实现
+let template = '我是{{name}}，年龄{{age}}，性别{{sex}}';
+let data = {
+  name: '姓名',
+  age: 18
+}
+render(template, data); // 我是姓名，年龄18，性别undefined
+
+function render(template, data) {
+	let reg = /\{\{(\w+)\}\}/;
+	if (reg.test(template)) {
+		const name = reg.exec(template)[1];
+		template = template.replace(reg, data[name]);
+		return render(template, data);
+	}
+	return template;
+}
